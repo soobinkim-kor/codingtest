ffa5569763c03febefa515732b3943e63052cb1a
soobinkim-kor <shb03207@naver.com>
2022-11-03 18:02:02 +0900

    백준 - 11725

--- src/백준/baek11725.java
+++ src/백준/baek11725.java
@@ -11,42 +11,85 @@ public class baek11725 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayList<int[]> arrayList = new ArrayList<>();
-        int[] answer = new int[n];
-        Queue<int[]> queue = new LinkedList<>();
+        //ArrayList<int[]> arrayList = new ArrayList<>();
+        int[] answer = new int[n+1];
+        //Queue<int[]> queue = new LinkedList<>();
+
+        // [[0번 노드와 연결된 애들], [1번 노드와 연결된 애들], [2번 노드와 연결된 애들], ... [n번 노드와 연결된 애들] ]
+        ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
+        boolean[] visited = new boolean[n+1]; // 0부터 n 까지 방문
+        for(int i=0;i<n+1;i++){
+            arrayList.add(new ArrayList<Integer>());
+        }
         for(int i=0;i<n-1;i++){
             String[] info = br.readLine().split(" ");
             int start = Integer.parseInt(info[0]);
             int end = Integer.parseInt(info[1]);
-            arrayList.add(new int[]{start,end});
+            arrayList.get(start).add(end);
+            arrayList.get(end).add(start);
+            //arrayList.add(new int[]{start,end});
         }
-        boolean[] visited = new boolean[n];
+        visited[1]=true;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        while(!queue.isEmpty()){
+            int num = queue.poll();
+            ArrayList<Integer> connections = arrayList.get(num);
+            for(int connection : connections){
+                if(!visited[connection]){
+                    visited[connection]=true;
+                    answer[connection]=num;
+                    queue.add(connection);
+                }
+            }
+        }
+        /* 이렇게 하면 안되고 ArrayList<ArrayList<Integer>> 로 해야함
+            1번째 = 1과 연결된 모든 노드(숫자) 들
+        *
+        * */
 
+
+        //boolean[] visited = new boolean[n-1];
+
+
+        /*
         queue.add(new int[]{0,1});
+
         while(!queue.isEmpty()){
             //printQueue(queue);
             int[] data = queue.poll();
-            int start = data[0];
             int end = data[1];
 
             for(int i=0;i<arrayList.size();i++){
+                // 각 연걸내용 마다 실행
+
+                // 방문했던 노드 이면
+                if(visited[i]){
+                    continue;
+                }
+
                 int[] info = arrayList.get(i);
-                if(end==info[0] && !visited[info[1]-1]){
-                    visited[info[1]-1]=true;
+                //System.out.println("이번 꺼: "+info[0]+" "+info[1]);
+                if(end==info[0]){
+                    visited[i]=true;
                     answer[info[1]-1]=end;
                     queue.add(new int[]{end,info[1]});
                 }
-                else if(end==info[1] && !visited[info[0]-1]){
-                    visited[info[1]-1]=true;
+                else if(end==info[1]){
+                    visited[i]=true;
                     answer[info[0]-1]=end;
                     queue.add(new int[]{end,info[0]});
                 }
             }
         }
 
-        for(int d : answer){
-            System.out.println(d);
+        */
+        StringBuilder sb = new StringBuilder();
+        for(int i=2;i<answer.length;i++){
+            sb.append(answer[i]).append("\n");
         }
+        System.out.println(sb);
+
 
     }
 
