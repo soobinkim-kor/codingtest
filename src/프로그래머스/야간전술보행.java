0e5626b5b4ab6b6f8f5f6dd6740b6824a0513864
soobinkim-kor <shb03207@naver.com>
2022-11-15 21:59:20 +0900

    프로그래머스 - 야간전술보행

--- src/프로그래머스/야간전술보행.java
+++ src/프로그래머스/야간전술보행.java
@@ -1,4 +1,13 @@
 package 프로그래머스;
 
 public class 야간전술보행 {
+    public static void main(String[] args) {
+        int[][] scope ={{3, 4},{5, 8}};
+        int[][] times = {{2,5},{4,3}};
+        System.out.println(solution(10,scope,times));
+    }
+    public static int solution(int distance, int[][] scope, int[][] times) {
+        int answer = 0;
+        return answer;
+    }
 }
