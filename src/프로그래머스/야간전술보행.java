ef54fc2f384923dd29b9e86e33b9b6b563ec1e39
soobinkim-kor <shb03207@naver.com>
2022-11-18 01:08:40 +0900

    프로그래머스 - 야간전술보행

--- src/프로그래머스/야간전술보행.java
+++ src/프로그래머스/야간전술보행.java
@@ -8,17 +8,19 @@ public class 야간전술보행 {
     // 거리 + 휴식 시간의 나머지 로
     static int current;
     public static void main(String[] args) {
-        int[][] scope = {{3,4},{5,8}};
-        int[][] times = {{2,5},{4,3}};
+        int[][] scope = {{3,4},{5,8},{3,2}};
+        int[][] times = {{2,5},{4,3},{1,2}};
 
         int[][] scope2 = {{7,8},{4,6},{11,10}};
         int[][] times2 = {{2,2},{2,4},{3,3}};
-        //System.out.println(solution(10,scope,times));
+        System.out.println(solution(10,scope,times));
+        System.out.println("---------------------");
         System.out.println(solution(12,scope2,times2));
     }
     public static int solution(int distance, int[][] scope, int[][] times) {
         HashMap<Integer,int[]> guards = new HashMap<>();
         for(int i=0;i<scope.length;i++){
+
             guards.put(i,scope[i]);
         }
 
@@ -32,25 +34,29 @@ public class 야간전술보행 {
 
         for(int guard : sortedTimes){
             // guard = 경비병의 번호
-            //System.out.println("경비병 번호: "+guard);
+            System.out.println("경비병 번호: "+guard);
+            int a = scope[guard][0];
+            int b = scope[guard][1];
+            current= Math.min(a,b);
+            int start = Math.min(a,b);
+            int end = Math.max(a,b);
 
-            current= scope[guard][0];
-            int end = scope[guard][1];
-            //System.out.println("목표 위치 : "+end);
-            //System.out.println("여기서 시작: "+current);
+            System.out.println("경비병의 끝 위치 : "+end);
+            System.out.println("현재 화랑이의 위치: "+current);
             int[] guardData = guards.get(guard);
-            if(!isPresent(guardData,guard,times)){
+            if(!isPresent(guardData,guard,times,start,end)){
                 return current;
             }
-            // 감시 시작 구간
+
         }
+        System.out.println("더 이상 경비병 없음");
 
 
         return distance;
     }
 
     // 정해진 시간에, 그 경비병이 휴식하지 않는 시간이 있는지 확인하는 메소드
-    public static boolean isPresent(int[] guardData, int guard, int[][] times){
+    public static boolean isPresent(int[] guardData, int guard, int[][] times, int start, int end){
         // 우선 진입 시간으로 확인
         // ex current 10초, 경비병 휴식 [근무: 2초, 휴식: 4초] -> 10/6 = 1세트 , 나머지 = 4 = 현재 시간에 경비 근무/휴식의 어느 부분에 있는지.
         // 나머지 - 근무 > 0 -> 근무 시간 지난 거. 휴식 중
@@ -63,53 +69,41 @@ public class 야간전술보행 {
         // 4초에 근무 시작 , 4초에 있으면 잡힘
 
         // 감시 시작 구간
-        int guardStart = guardData[0];
-        // 감시 끝 구간
-        int guardEnd = guardData[1];
 
-        int pass = guardEnd-guardStart;
-        //System.out.println("화랑이가 통과하려면 "+pass+" 초 지나가야함");
+        int pass = end-start;
+        System.out.println("화랑이가 통과하려면 "+pass+" 초 지나가야함");
         // 근무 시간
         int guardActive = times[guard][0];
         // 휴식 시간
         int guardRest = times[guard][1];
-        //System.out.println("해당 경비병의 근무 시간: "+guardActive+" 휴식 시간: "+guardRest);
+        System.out.println("해당 경비병의 근무 시간: "+guardActive+" 휴식 시간: "+guardRest);
         // 경비원이 (휴식+근무를) 몇 세트 했는지
         //int startRest = current/(guardActive+guardRest);
         // 현재 시간에 경비 근무/휴식의 어느 부분에 있는지.
         int time = current%(guardActive+guardRest);
-        //System.out.println("현재 경비병은 의 위치: "+time);
-        if(time-guardActive<0){
-            //System.out.println("진입 시 경비병이 근무 중");
+        System.out.println("현재 경비병은 의 위치: "+time);
+        if(time-guardActive<=0){
+            System.out.println("진입 시점에 경비병이 근무 중");
             return false;
         }
 
         int activeLeft = guardActive+guardRest-time+1;
-        //System.out.println("현재 경비병은 "+activeLeft+" 초 후 근무 시작");
-        if(activeLeft<=pass){
-            //System.out.println("지나는 중 잡힘");
+        System.out.println("경비병은 "+(current+activeLeft)+" 초에 근무 시작");
+        if(current+activeLeft<=end){
+            System.out.println("지나는 중 잡힘");
             current+=activeLeft;
+            System.out.println(current+" 까지 가서 잡혔음");
             return false;
         }
         else{
-            //System.out.println("무사히 통과");
+            System.out.println("무사히 통과");
             return true;
         }
     }
 
     public static ArrayList<Integer> sortArray(HashMap<Integer,int[]> guards){
         ArrayList<Integer> guardsTime = new ArrayList<>(guards.keySet());
-        guardsTime.sort(Comparator.comparingInt(g -> guards.get(g)[0]));
+        guardsTime.sort(Comparator.comparingInt(g -> Math.min(guards.get(g)[0], guards.get(g)[1])));
         return guardsTime;
-    }
-
-    public static void print(int[][] data){
-        for(int[] d : data){
-            for(int a : d){
-                System.out.print(a+" ");
-            }
-            System.out.println();
-        }
-        System.out.println();
     }
 }
