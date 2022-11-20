643439ca1e2b2db4764e8e99e82040ff826ba11e
soobinkim-kor <shb03207@naver.com>
2022-11-20 14:47:49 +0900

    프로그래머스 - 입국심사

---
+++ src/프로그래머스/입국심사.java
@@ -0,0 +1,17 @@
+package 프로그래머스;
+
+import java.util.Arrays;
+
+public class 입국심사 {
+    public static void main(String[] args) {
+        int n=6;
+        int[] times = {7,10};
+        System.out.println(solution(n,times));
+    }
+
+    public static long solution(int n, int[] times) {
+        Arrays.sort(times);
+        long answer = 0;
+        return answer;
+    }
+}
