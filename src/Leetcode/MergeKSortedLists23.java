c4abbc7b04ee25d69f5612fbb0c6daaf9ae83c38
Soobin <shb03207@naver.com>
2024-09-24 23:27:52 +0900

    Leetcode Merge K Sorted Linked Lists

---
+++ src/Leetcode/MergeKSortedLists23.java
@@ -0,0 +1,14 @@
+package Leetcode;
+
+public class MergeKSortedLists23 {
+    class Solution {
+        public ListNode mergeKLists(ListNode[] lists) {
+
+            return null;
+        }
+    }
+
+
+
+
+}
